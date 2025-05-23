db21e20de8b69a9df5a1d8d6eaafa9547895ab81
putri-isneini <isnainiputri098@gmail.com>
2025-05-23 08:52:52 +0700

    ini main ya

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from './components/MainLayout'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route element={<MainLayout/>} >
+      <Route path="/" element={<Dashboard/>}/>
+    </Route>
+    </Routes>
+  )
+}
+
+export default App
